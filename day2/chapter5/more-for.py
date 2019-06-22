26019bb21c7cabd2f453737b74ee0459d1703a8c
Abimbola Olaniran <israelolaniran@gmail.com>
2019-06-22 18:13:01 +0100

    last entry

---
+++ day2/chapter5/more-for.py
@@ -0,0 +1,12 @@
+# to print even numbers in a range
+
+for x in range(1, 21):
+    if x % 2 ==0:
+        print(x)
+
+
+#to add numbers within a range
+total = 0
+for x in range(1, 5):
+    total += x
+print('total is', total)
---
+++ day2/chapter5/nested.py
@@ -0,0 +1,10 @@
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+for x in adj:
+    for y in fruits:
+        print(x, y)
+
+
+for x in range(2,5):
+    for y in range(8, 10):
+        print(x*y)
